ebae6d9e175815b176a6c1deeb45bda69c5c0047
Blaine Curren <blaine.curren@gmail.com>
2022-12-27 05:07:43 -0600

    added useProductReducer function

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,5 @@
+import { useReducer } from "react";
+
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
@@ -28,3 +30,7 @@ export const reducer = (state, action) => {
       return state;
   }
 };
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
