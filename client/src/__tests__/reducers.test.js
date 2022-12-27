b5de112f3b667deba331cc54735cd9570c09e420
Blaine Curren <blaine.curren@gmail.com>
2022-12-27 02:33:55 -0600

    added tests folder and initialState test

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,21 @@
+import {
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from "...utils/actions";
+
+const initialState = {
+  products: [],
+  categories: [{ name: "Food" }],
+  currentCategory: "1",
+};
+
+test("UPDATE_PRODUCTS", () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_PRODUCTS,
+    products: [{}, {}],
+  });
+
+  expect(newState.products.length).toBe(2);
+  expect(initialState.products.length).toBe(0);
+});
