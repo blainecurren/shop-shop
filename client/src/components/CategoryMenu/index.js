06a5239e0df6c8d3040670979afaa37d301a5885
Blaine Curren <blaine.curren@gmail.com>
2022-12-28 22:37:40 -0600

    restarted mongod

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -8,7 +8,7 @@ import {
   UPDATE_CURRENT_CATEGORY,
 } from "../../utils/actions";
 
-function CategoryMenu({}) {
+function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
   const { categories } = state;
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
