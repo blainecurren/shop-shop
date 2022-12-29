35a534aa670c7ae8cc999d7a97a63e2296fbe9be
Blaine Curren <blaine.curren@gmail.com>
2022-12-29 10:08:22 -0600

    added calculateTotal / cart.length / cart.map to cart jsx

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -36,33 +36,28 @@ const Cart = () => {
         [close]
       </div>
       <h2>Shopping Cart</h2>
-      <div>
-        <CartItem
-          item={{
-            name: "Camera",
-            image: "camera.jpg",
-            price: 5,
-            purchaseQuantity: 3,
-          }}
-        />
-        <CartItem
-          item={{
-            name: "Soap",
-            image: "soap.jpg",
-            price: 6,
-            purchaseQuantity: 4,
-          }}
-        />
-
-        <div className="flex-row space-between">
-          <strong>Total: $0</strong>
-          {Auth.loggedIn() ? (
-            <button>Checkout</button>
-          ) : (
-            <span>(log in to checkout)</span>
-          )}
+      {state.cart.length ? (
+        <div>
+          {state.cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {Auth.loggedIn() ? (
+              <button>Checkout</button>
+            ) : (
+              <span>(log in to check out)</span>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
     </div>
   );
 };
