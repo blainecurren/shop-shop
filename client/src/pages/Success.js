a6a0a10e2d3e30f3e29226a3608fa7f7b3bf431f
Blaine Curren <blaine.curren@gmail.com>
2023-01-04 08:59:06 -0600

    set timeout added to success.js

---
+++ client/src/pages/Success.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import Jumbotron from "../components/Jumbotron";
+import { ADD_ORDER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+
+function Success() {
+  const [addOrder] = useMutation(ADD_ORDER);
+
+  useEffect(() => {
+    async function saveOrder() {
+      // //   const products = cart.map((item) => item._id);
+      //   if (products.length) {
+      //     const { data } = await addOrder({ variables: { products } });
+      //     // const productData = data.addOrder.products;
+      //   }
+    }
+
+    setTimeout(() => {
+      window.location.assign("/");
+    }, 3000);
+
+    saveOrder();
+  }, [addOrder]);
+
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Success!</h1>
+        <h2>Thank you for your purchase</h2>
+        <h2>You will now be redirected to the homepage</h2>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default Success;
